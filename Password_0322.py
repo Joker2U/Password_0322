8b0c3c6d98201305e1233c7ced677e7e246dff50
Joker2U <yngwieli10@gmail.com>
2026-03-22 10:12:58 +0800

    更新密碼功能 v2

--- Password_0322.py
+++ Password_0322.py
@@ -1,11 +1,13 @@
 x = 3
-while x < 4:  
+while x > 0:  
+    x = x -1
     password = input("請輸入密碼:")
     if password == '1234':
         print("密碼正確")
     else :
-        print(f"密碼錯誤還有{x}次機會")
-        x = x -1
-        if x == 0:
-            print("無法再嘗試程式結束")
+        print("密碼錯誤")
+        if x > 0:
+            print(f"密碼錯誤還有{x}次機會")
+        
+
             
